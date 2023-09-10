fd4bfef48899b4327e6cf8578d5a04c3944c7a20
Mohamed Khaled <81655815+MohamedKhaled111120@users.noreply.github.com>
2023-09-10 17:55:29 +0300

    LCD Driver Uploaded

---
+++ HAL/LCD/LCD_Private.h
@@ -0,0 +1,20 @@
+/*
+ * LCD_Private.h
+ *
+ *  Created on: Sep 9, 2023
+ *      Author: khale
+ */
+
+#ifndef REPO_HAL_LCD_LCD_PRIVATE_H_
+#define REPO_HAL_LCD_LCD_PRIVATE_H_
+
+
+#define GPIOA	0
+#define GPIOB	1
+
+
+#define EIGHT_BITS_MODE	1
+#define FOUR_BITS_MODE	0
+
+
+#endif /* REPO_HAL_LCD_LCD_PRIVATE_H_ */
